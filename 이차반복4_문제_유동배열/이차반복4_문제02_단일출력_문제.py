4a63e634026fdb23dcaf93545da24a3513d33d6d
JJY <jyjang1222@naver.com>
2022-11-15 17:41:28 +0900

    이차반복4 문제풀이 일부업데이트

--- 이차반복4_문제_유동배열/이차반복4_문제02_단일출력_문제.py
+++ 이차반복4_문제_유동배열/이차반복4_문제02_단일출력_문제.py
@@ -7,3 +7,17 @@
 '''
 a = [1,1,2,2,3,3,4,100,3]
 
+
+for i in range(len(a)):
+	chk = True
+	# 현재인덱스i에서 이전 배열값 모두 비교
+	for j in range(i):
+		if a[i] == a[j]:
+			chk = False
+			break
+	if chk:
+		print(a[i], end = " ")
+
+
+
+	
--- 이차반복4_문제_유동배열/이차반복4_문제03_로또정렬_문제.py
+++ 이차반복4_문제_유동배열/이차반복4_문제03_로또정렬_문제.py
@@ -1,3 +1,4 @@
+import random
 '''
 	[문제] 
 		1~45사이의 랜덤값 6개를 lotto에 저장하려 한다.
@@ -7,3 +8,40 @@
         [40, 38, 27, 26, 18, 5]
 '''
 lotto = []
+r = random.randint(1, 6)
+lotto.append(r)
+
+
+i = 1
+while True:
+	if len(lotto) == 6:
+		break
+	r = random.randint(1, 6)
+	chk = True
+	# r값이 중복되는지 체크
+	for j in range(len(lotto)):
+		if lotto[j] == r:
+			chk = False
+			break
+	if chk:
+		lotto.append(r)
+	i += 1
+
+print(lotto)
+
+# 정렬
+tmp = 0
+# 비교는 마지막인덱스-1 까지 가능
+for idx in range(len(lotto) - 1):
+	max = 0
+	for i in range(idx, len(lotto)):
+		if lotto[i] > max:
+			max = lotto[i]
+	for j in range(len(lotto)):
+		if lotto[j] == max:
+			tmp = lotto[idx]
+			lotto[idx] = lotto[j]
+			lotto[j] = tmp
+
+print(lotto)
+
