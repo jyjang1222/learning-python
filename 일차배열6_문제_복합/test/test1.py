6865257a45426849b66c7eba4ed91780cb5dde50
JJY <jyjang1222@naver.com>
2022-11-18 13:33:47 +0900

    221118테스트 풀이추가업데이트

--- 일차배열6_문제_복합/test/test1.py
+++ 일차배열6_문제_복합/test/test1.py
@@ -19,6 +19,7 @@ while True:
 
     if r % 2 == 0:
         continue
+
     for i in range(len(a)):
         if a[i] == r:
             chk = False
@@ -26,3 +27,43 @@ while True:
         a.append(r)
 
 print(a)
+
+# 1차반복문만 사용
+a = []
+i = 0
+chk = True
+while True:
+    r = random.randint(1, 30)
+    if r % 2 == 1:
+        a.append(r)
+        break
+
+while True:
+    if len(a) == 10:
+        break
+
+    # 홀수짝수체크
+    if r % 2 == 0:
+        r = random.randint(1, 30)
+        continue
+    
+    if i == len(a) and len(a) > 1:
+        i = 0
+        chk = True
+        r = random.randint(1, 30)
+    if len(a) == 1 and a[0] == r:
+        r = random.randint(1, 30)
+        continue
+    
+    # 중복숫자가 있으면 chk = False
+    if len(a) > 1 and a[i] == r:
+        chk = False
+    # 중복숫자가 없으면 chk = true
+    if i == len(a) - 1 and chk == True:
+        a.append(r)
+        chk = False
+
+    i += 1
+
+print(a)
+
