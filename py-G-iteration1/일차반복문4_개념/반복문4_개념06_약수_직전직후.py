12016b3ee0ab159200b4a25d0a8b9d998aa7189d
JJY <jyjang1222@naver.com>
2023-01-03 16:39:10 +0900

    엘리베이터 문제풀이 업데이트

--- py-G-iteration1/일차반복문4_개념/반복문4_개념06_약수_직전직후.py
+++ py-G-iteration1/일차반복문4_개념/반복문4_개념06_약수_직전직후.py
@@ -10,20 +10,20 @@
 
 # 풀이 (푸는데 시간 좀 걸렸음)
 
-num = 0
-num2 = 0
-i = 1
+# num = 0
+# num2 = 0
+# i = 1
 
-while i <= 280:
-    if i < 50 and 280 % i == 0:
-        num = i
-    if i > 50 and 280 % i == 0:
-        num2 = i
-        break
-    i += 1
+# while i <= 280:
+#     if i < 50 and 280 % i == 0:
+#         num = i
+#     if i > 50 and 280 % i == 0:
+#         num2 = i
+#         break
+#     i += 1
     
-print(num, num2)
-
+# print(num, num2)
+# 정답
 # front = 0
 # back = 0
 
@@ -40,4 +40,37 @@ print(num, num2)
 # print(back)
 
 
+# 2트
+'''
+[문제] 
+    280의 약수 중에
+    50바로 전의 값과 바로 뒤의 값을 구하시오.
+    만약 50이 약수이면, 바로 뒤의 값은 50이다.
+[정답]
+    40
+    56
+'''
 
+n = 280
+ans = 0
+for i in range(1, n+1):
+    if n % i == 0:
+        if i < 50:
+            ans = i
+        if i > 50:
+            print(ans)
+            ans = i
+            print(ans)
+            break
+
+n = 280
+arr = []
+for i in range(1, n+1):
+    if n % i == 0:
+        arr.append(i)
+
+for i in range(len(arr)):
+    if arr[i] > 50:
+        print(arr[i-1])
+        print(arr[i])
+        break
--- py-G-iteration1/일차반복문7_문제/반복문7_문제02_건물주_문제.py
+++ py-G-iteration1/일차반복문7_문제/반복문7_문제02_건물주_문제.py
@@ -59,23 +59,74 @@
 		50 66
 '''
 
-i = 1
-floor = 1
-count = 0
+# i = 1
+# floor = 1
+# count = 0
 
-while i <= 50:
-	if i // 10 == 4:
-		if floor % 10 == 4:
-			floor += 1
-		print(i, floor)
-		floor += 1
-		i += 1
-		continue
-	if i % 10 == 4 :
-		floor += 1
-	if floor == 40:
-		floor += 10
-	print(i, floor)
+# while i <= 50:
+# 	if i // 10 == 4:
+# 		if floor % 10 == 4:
+# 			floor += 1
+# 		print(i, floor)
+# 		floor += 1
+# 		i += 1
+# 		continue
+# 	if i % 10 == 4 :
+# 		floor += 1
+# 	if floor == 40:
+# 		floor += 10
+# 	print(i, floor)
 
-	floor += 1
-	i += 1
+# 	floor += 1
+# 	i += 1
+
+# 2트
+# fakeFloor = 1
+# for realFloor in range(1, 51):
+# 	n = fakeFloor
+# 	chk = False
+# 	while n != 0:
+# 		if n % 10 == 4:
+# 			chk = True
+# 		n //= 10
+# 	if chk:
+# 		fakeFloor += 1
+# 	print(realFloor, fakeFloor)
+# 	if fakeFloor == 39:
+# 		fakeFloor += 9
+# 	fakeFloor += 1
+
+
+# 3트
+# fakeFloor = 1
+# realFloor = 1
+
+# while realFloor <= 50:
+# 	if fakeFloor % 10 == 4:
+# 		fakeFloor += 1
+# 	if fakeFloor // 10 == 4:
+# 		fakeFloor += 10
+# 	print(realFloor, fakeFloor)
+# 	fakeFloor += 1
+# 	realFloor += 1
+
+# 4트
+realFloor = 1
+elevator = 1
+
+while realFloor <= 500:
+	n = elevator
+	total = 0
+	count = 1
+	# 464 43 4
+	while n != 0:
+		if n % 10 == 4:
+			total += count
+		count *= 10
+		n //= 10
+
+	elevator += total
+
+	print(realFloor, elevator)
+	elevator += 1
+	realFloor += 1
