37c7027e2f5bcd72208be76a141748a0bcb2e2b5
JJY <jyjang1222@naver.com>
2023-02-10 17:24:58 +0900

    상속 업데이트

--- py-N-class/클래스1_개념_기본/practice_class.py
+++ py-N-class/클래스1_개념_기본/practice_class.py
@@ -14,6 +14,7 @@ cat1.color = 'black'
 cat1.printInfo()
 cat1.cry()
 
+# 생성자 연습
 class Dog:
     def __init__(self, a, b, c):
         self.name = a
@@ -28,3 +29,25 @@ dog1 = Dog('myDog1', 1, 'brown')
 dog1.printInfo()
 dog1.cry()
 # dog2 = Dog() -> 에러발생
+
+# 상속연습
+class Animal:
+    def __init__(self, a, b, c):
+        self.name = a
+        self.age = b
+        self.color = c
+    def printInfo(self):
+        print(self.name, self.age, self.color)
+class Owl(Animal):
+    def cry(self):
+        print(self.name, ': 부엉 ~')
+class Wolf(Animal):
+    def cry(self):
+        print(self.name, ': 아우 ~')
+
+owl1 = Owl('myOwl1', 2, 'white')
+owl1.printInfo()
+owl1.cry()
+wolf1 = Wolf('myWolf1', 3, 'gray')
+wolf1.printInfo()
+wolf1.cry()
